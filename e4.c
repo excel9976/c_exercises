b0f9411c778d8568804e2940bc25c7b0c71a2586
Nicole Fontenot <nfontenot27@gmail.com>
2018-01-13 20:31:25 -0600

    spelling and give more hints

--- e4.c
+++ e4.c
@@ -3,9 +3,11 @@
 
 int main(void)
 {
-	//print the digits seperated by a space.
+	//print the digits separated by a space.
 
+	int digits_len = 10; //for convince
 	int digits[] = {27, 28, 14, 22, 24, 35, 3, 18, 10, 14};
 
 	//put a newline at the end of output
+	return 0;
 }
