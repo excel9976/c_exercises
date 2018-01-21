74e77da10773e4577ff1640c07d311eff076a159
Nicole Fontenot <nfontenot27@gmail.com>
2018-01-20 20:44:06 -0600

    Add student's work to the repo(this will be force-removed)

---
+++ fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	// let x be one integer from the command line.
+	// for each n in the range 1 and x inclusive
+	// 
+	// if n divisible by 3 print "fizz\n"
+	// if n divisible by 5 print "buzz\n"
+	// if n divisible by 15 print "fizzbuzz\n"
+int x = 0,
+    while(x < 10);
+	{
+	printf ("%d", x);
+		x = x + 1;
+break;
+	};
+
+	// good luck!
+	return 0;
+}
